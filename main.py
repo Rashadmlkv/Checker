c69e803037b844a3d88123b1d9e5f0303c0b083d
Rashadmlkv <resadmlkv@gmail.com>
2024-06-18 17:08:45 +0400

    upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# 0 - empty
-# 1 - black , 3 - king
+# 0 - white square, 1 - black square
+# 3 - black , 5 - king
 # 2 - white , 4 - king
 
 running = True
@@ -12,11 +12,11 @@ turn = 0
 
 #create board
 board = [
-         [0, 1, 0, 1, 0, 1, 0, 1],
+         [0, 3, 0, 3, 0, 3, 0, 3],
+         [3, 0, 3, 0, 3, 0, 3, 0],
+         [0, 3, 0, 3, 0, 3, 0, 3],
          [1, 0, 1, 0, 1, 0, 1, 0],
          [0, 1, 0, 1, 0, 1, 0, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0],
          [2, 0, 2, 0, 2, 0, 2, 0],
          [0, 2, 0, 2, 0, 2, 0, 2],
          [2, 0, 2, 0, 2, 0, 2, 0]]
@@ -35,29 +35,29 @@ while running:
         if (board[a][b] == 2):
             c, d = [int(a) for a in input("Select square: ").split()]
             #move
-            if (c == a - 1 and (d == b - 1 or d == b + 1) and board[c][d] == 0):
-                board[a][b] = 0
+            if (c == a - 1 and (d == b - 1 or d == b + 1) and board[c][d] == 1):
+                board[a][b] = 1
                 if c == 0:
                     board[c][d] = 4
                 else:
                     board[c][d] = 2
             #attack
-            elif ((c == a - 2 or c == a + 2) and board[c][d] == 0):
+            elif ((c == a - 2 or c == a + 2) and board[c][d] == 1):
                 #left attack
                 if d == b - 2:
                     #forward
-                    if c == a - 2 and board[a-1][b-1] == 1:
+                    if c == a - 2 and (board[a-1][b-1] == 3 or board[a-1][b-1] == 5):
                         if c == 0:
                             board[c][d] = 4
                         else:
                             board[c][d] = 2
-                        board[a][b] = 0
-                        board[a-1][b-1] = 0
+                        board[a][b] = 1
+                        board[a-1][b-1] = 1
                     #back
-                    elif board[a+1][b-1] == 1:
+                    elif board[a+1][b-1] == 3 or board[a+1][b-1]==5:
                         board[c][d] = 2
-                        board[a][b] = 0
-                        board[a+1][b-1] = 0
+                        board[a][b] = 1
+                        board[a+1][b-1] = 1
                     else:
                         print("can't attack")
                         continue
@@ -65,27 +65,27 @@ while running:
                 #right attack
                 if d == b + 2:
                     #forward
-                    if c == a - 2 and board[a-1][b+1] == 1:
+                    if c == a - 2 and board[a-1][b+1] == 3 or board[a-1][b+1] == 5:
                         if c == 0:
                             board[c][d] = 4
                         else:
                             board[c][d] = 2
-                        board[a][b] = 0
-                        board[a-1][b+1] = 0
+                        board[a][b] = 1
+                        board[a-1][b+1] = 1
                     #back
-                    elif board[a+1][b+1] == 1:
+                    elif board[a+1][b+1] == 3 or board[a+1][b+1] == 5:
                         board[c][d] = 2
-                        board[a][b] = 0
-                        board[a+1][b+1] = 0
+                        board[a][b] = 1
+                        board[a+1][b+1] = 1
                     else:
                         print("can't attack")
                         continue
 
                 #continue attack
-                if (board[c+1][d-1] == 1 and board[c+2][d-2] == 0   #left bottom
-                 or board[c+1][d+1] == 1 and board[c+2][d+2] == 0   #right bottom
-                 or board[c-1][d-1] == 1 and board[c-2][d-2] == 0   #left upper
-                 or board[c-1][d+1] == 1 and board[c-2][d+2] == 0): #right upper
+                if (board[c+1][d-1] == 3 or board[c+1][d-1] == 5 and board[c+2][d-2] == 1   #left bottom
+                 or board[c+1][d+1] == 3 or board[c+1][d+1] == 5 and board[c+2][d+2] == 1   #right bottom
+                 or board[c-1][d-1] == 3 or board[c-1][d-1] == 5 and board[c-2][d-2] == 1   #left upper
+                 or board[c-1][d+1] == 3 or board[c-1][d+1] == 5 and board[c-2][d+2] == 1): #right upper
                         for i in board:
                             print(i)
                         continue
--- menu.py
+++ menu.py
@@ -4,18 +4,18 @@ import pygame
 
 
 board = [
-         [0, 1, 0, 1, 0, 1, 0, 1],
+         [0, 3, 0, 3, 0, 3, 0, 3],
+         [3, 0, 3, 0, 3, 0, 3, 0],
+         [0, 3, 0, 3, 0, 3, 0, 3],
          [1, 0, 1, 0, 1, 0, 1, 0],
          [0, 1, 0, 1, 0, 1, 0, 1],
-         [0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0],
          [2, 0, 2, 0, 2, 0, 2, 0],
          [0, 2, 0, 2, 0, 2, 0, 2],
          [2, 0, 2, 0, 2, 0, 2, 0]]
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 running = True
 posx = 0
@@ -35,14 +35,30 @@ while running:
     # RENDER YOUR GAME HERE
 
     for i in range(8):
-        for j in range(4):
-            pygame.draw.rect(screen, color, (posx, posy, width, height))
-            posx += 100
+        for j in range(8):
+            if board[i][j] == 0:
+                pygame.draw.rect(screen, (255,255,255), (posx, posy, width, height))
+                posx += 50
+            elif board[i][j] == 1:
+                pygame.draw.rect(screen, (255,0,0), (posx, posy, width, height))
+                posx += 50
+            elif board[i][j] == 3:
+                pygame.draw.rect(screen, (255,0,0), (posx, posy, width, height))
+                pygame.draw.circle(screen, (0,0,0), (posx + 25, posy + 25), 13)
+                posx += 50
+            elif board[i][j] == 2:
+                pygame.draw.rect(screen, (255,0,0), (posx, posy, width, height))
+                pygame.draw.circle(screen, (255,255,255), (posx + 25, posy + 25), 13)
+                posx += 50
+
         posy += 50
-        if (i % 2 != 0):
-            posx = 0
-        else: posx = 50
+        posx = 0
+
+
+
     # flip() the display to put your work on screen
+
+
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
