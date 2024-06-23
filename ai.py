08e03c6dbaec8b1a1d1e4c1e73a259b513a13c8c
Rashadmlkv <resadmlkv@gmail.com>
2024-06-23 22:57:35 +0400

    upload

--- ai.py
+++ ai.py
@@ -27,17 +27,31 @@ def get_valid_moves(board, a, b):
                 valid_moves.append((a+2, b+2))
     return valid_moves
 
+
+
 def ai_make_move(board):
-    # Get all valid pieces for the AI (black pieces)
     valid_pieces = [(i, j) for i in range(8) for j in range(8) if board[i][j] == 3]
     random.shuffle(valid_pieces)
     
     for a, b in valid_pieces:
         valid_moves = get_valid_moves(board, a, b)
-        if valid_moves:
+        
+        attack_moves = [(c, d) for c, d in valid_moves if abs(c - a) == 2]
+        
+        if attack_moves:
+            for c, d in attack_moves:
+                mid_x = (a + c) // 2
+                mid_y = (b + d) // 2
+                board[a][b] = 1
+                board[c][d] = 3
+                board[mid_x][mid_y] = 1 
+                return True
+        
+        elif valid_moves:
             c, d = random.choice(valid_moves)
             board[a][b] = 1
             board[c][d] = 3
+            
             return True
+    
     return False
-
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import draw
+import draw, ai
 import pygame, sys, time
 
 # 0 - white square, 1 - black square
@@ -140,74 +140,11 @@ while running:
 
 
     elif turn == 1:
-        ##a, b = [int(a) for a in input("Select piece: ").split()]
-        #is correct piece selected
-        if (board[a][b] == 3):
-            if pygame.mouse.get_pressed() == (True, False, False):
-                c, d = get_pos(pygame.mouse.get_pos())
-            ##c, d = [int(a) for a in input("Select square: ").split()]
-            #move
-            if (c == a + 1 and (d == b - 1 or d == b + 1) and board[c][d] == 1):
-                board[a][b] = 1
-                board[c][d] = 3
-                play_sound()
-            #attack
-            elif ((c == a - 2 or c == a + 2) and board[c][d] == 1):
-                #left attack
-                if d == b - 2:
-                    #forward
-                    if c == a + 2 and board[a+1][b-1] == 2:
-                        board[c][d] = 3
-                        board[a][b] = 1
-                        board[a+1][b-1] = 1
-                        play_sound()
-                    #back
-                    elif board[a-1][b-1] == 2:
-                        board[c][d] = 3
-                        board[a][b] = 1
-                        board[a-1][b-1] = 1
-                        play_sound()
-                    else:
-                        print("can't attack")
-                        continue
-
-                #right attack
-                if d == b + 2:
-                    #forward
-                    if c == a + 2 and board[a+1][b+1] == 2:
-                        board[c][d] = 3
-                        board[a][b] = 1
-                        board[a+1][b+1] = 1
-                        play_sound()
-                    #back
-                    elif board[a-1][b+1] == 2:
-                        board[c][d] = 3
-                        board[a][b] = 1
-                        board[a-1][b+1] = 1
-                        play_sound()
-                    else:
-                        print("can't attack")
-                        continue
-
-                #continue attack
-                if (c < 6 and d > 1 and board[c+1][d-1] == 2 and board[c+2][d-2] == 1   #left bottom
-                 or c < 6 and d < 6 and board[c+1][d+1] == 2 and board[c+2][d+2] == 1   #right bottom
-                 or c > 1 and d > 1 and board[c-1][d-1] == 2 and board[c-2][d-2] == 1   #left upper
-                 or c > 1 and d < 6 and board[c-1][d+1] == 2 and board[c-2][d+2] == 1): #right upper
-                    for i in board:
-                        print(i)
-                    continue
-            else:
-                print("can't move")
-                continue
-
-
-            for i in board:
-                print(i)
-
+        if ai.ai_make_move(board):
             turn = 0
         else:
-            print("Select black piece")
+            print("can;t move")
+
 
     clock.tick(30)  # limits FPS to 30
 
